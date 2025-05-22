7e87b3650127277f6ebf074398388506a964ded7
demetramitrica21 <roman.demetra21@gmail.com>
2025-05-22 20:39:12 +0300

    add homework requests

---
+++ src/test/java/homework/Telefon.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class Telefon {
+    //de facut un exemplu OOP asemanator cu exemplul de la curs;
+    //trebuie sa folosim:
+    //mostenire;
+    //incapsulare;
+    //polimorfism;
+    //abstractizare(clase abstracte si interfete);
+}
